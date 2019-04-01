f7acaed380b4f3e1cd0c3b1491e062ac5caa86c8
Miguel601 <henriquez.miguel@outlook.com>
2019-04-01 16:17:21 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+    get '/name' do
+        "My name is Miguel"
+    end
+
+    get '/hometown' do
+        "My hometown is Bronx,NY"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is So Appalled"
+    end
+
+
+
 end
