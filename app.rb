7708dda916d88948801d0e6fed6e696da6482293
tpr1992 <terence.pataneronan@gmail.com>
2019-04-01 10:40:46 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get('/') {"Hello, World!"}
+  get('/name') { "My name is Terence" }
+  get('/hometown') { "My hometown is Northport" }
+  get('/favorite-song') { "My favorite song is A Love Supreme" }
 end
